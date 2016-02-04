53520d3ff675ab524e541cc24a4f00a2b0a1e00d
FlashFrog <guissy@163.com>
2016-02-04 21:20:41 +0800

    fixed sth

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ import React from 'react';
 import sd from 'skin-deep';
 import {jsdom} from 'jsdom';
 jsdom
-import Main from '../app.jsx';
+import Main from '../main.jsx';
 // init jsdom
 
 document = global.document = jsdom();
@@ -13,6 +13,7 @@ global.navigator = global.window.navigator;
 describe("Main",()=>{
    it("#state.flowNodes not null", ()=>{
        const main = sd.shallowRender(React.createElement(Main,{}));
+       console.dir(main.text())
        //expect(main.state).to.exist();
        //expect(Object.size(main.state)).to.be(2);
        //expect(main.state).to.have.property('nodeMap').with.length.above(1);
